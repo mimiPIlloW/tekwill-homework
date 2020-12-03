5b31523c9772076ced3e0efd358e479a7e25d2b0
mimiPIllow <72455183+mimiPIlloW@users.noreply.github.com>
2020-12-03 22:49:39 +0200

    ChairTablePrinter FavoriteMovieQuote ShapePrinter StringSoutLines BigLettersMISP

---
+++ src/com/tekwill/learning/basics/BigLettersMISP.java
@@ -0,0 +1,14 @@
+package com.tekwill.learning.basics;
+
+public class BigLettersMISP {
+    public static void main(String[] args) {
+        System.out.println("M     M    IIIII    SSSSS    PPPPPP\n" +
+                "MM   MM      I     S     S   P     P\n" +
+                "M M M M      I     S         P     P\n" +
+                "M  M  M      I      SSSSS    PPPPPP\n" +
+                "M     M      I           S   P\n" +
+                "M     M      I     S     S   P\n" +
+                "M     M    IIIII    SSSSS    P\n");
+    }
+}
+
