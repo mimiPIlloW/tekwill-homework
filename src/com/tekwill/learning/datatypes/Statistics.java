a0d4d82ef266c08ad2eb8c3c9cdbe7122eb3a50e
mimiPIllow <72455183+mimiPIlloW@users.noreply.github.com>
2020-12-17 19:33:02 +0200

    StatisticWorldPopulation

--- src/com/tekwill/learning/datatypes/Statistics.java
+++ src/com/tekwill/learning/datatypes/Statistics.java
@@ -1,47 +1,22 @@
 package com.tekwill.learning.datatypes;
 
 public class Statistics {
-    private final long currentWorldPopulation;
-
-    private Statistics(long currentWorldPopulation1) {
-
-        this.currentWorldPopulation = currentWorldPopulation1;
-    }
-//    public Statistics(){
-//
-//    }
+    long population = 7831436009L;
+    int year = 2010;
+    double rateOfAround = 1.05;
+    String currentWorldPopulation = " Current World Population";
+    String world = "World Population Prospect the 2019 Revision - United Nations,\nDepartment of Economic and Social Affairs, Population Division\n(June 2019)";
+    String census = "International Programs Center at the U.S. Census Bureau,\nPopulation Division";
 
 
     public static void main(String[] args) {
-        Statistics sursa1 = new Statistics(7_831_436_009L);
-        System.out.println("WORLD POPULATION");
-        System.out.println(sursa1);
-        sursa1.sSursa1();
-//        Statistics sursa2 = new Statistics();
+        Statistics population = new Statistics();
+        System.out.println("World Population");
+        System.out.println(population.population + population.currentWorldPopulation);
+        System.out.println("is currently (" + population.year + ") growing at a rate of around " + population.rateOfAround + "% per year.");
+        System.out.println(population.world);
+        System.out.println(population.census);
+
     }
 
-
-    public void sSursa1() {
-        int b = 2020;
-        double d = 1.05;
-        System.out.println("is currently (" + b + ") growing at a rate of around " + d + "% per year.");
-    }
-
-//    public void sSursa2() {
-//        int b = 2020;
-//        double d = 1.05;
-//        System.out.println("is currently (" + b + ") growing at a rate of around " + d + "% per year.");
-//    }
-
-    @Override
-    public String toString() {
-        return currentWorldPopulation + " Current World Population";
-    }
 }
-
-
-//        double number = 7831436009.;
-//        DecimalFormat fDecimal = new DecimalFormat("#,###");
-//        System.out.println(fDecimal.format(number));
-//    }
-//}
